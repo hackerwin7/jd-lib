52dd1c85c56c8b742e1b74d7eeb3e4b83b8f81a9
hackerwin7 <1186897618@qq.com>
2016-02-02 16:34:41 +0800

    developing switch mysql source

---
+++ src/main/java/com/github/hackerwin7/jd/lib/executors/SwitchMysqlSource.java
@@ -0,0 +1,11 @@
+package com.github.hackerwin7.jd.lib.executors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hackerwin7
+ * Date: 2016/02/01
+ * Time: 6:07 PM
+ * Desc:
+ */
+public class SwitchMysqlSource {
+}
